df30d4a4bc115109630db19472e8703492263f22
Achyut Gupta <guptaachyut15@gmail.com>
2024-01-30 00:16:23 +0530

    Updated description and gitignore

--- apps/orders.py
+++ apps/orders.py
@@ -14,7 +14,7 @@ router = APIRouter(tags=["orders"])
 #     """
 
 
-@router.post("/")
+@router.post("/", description="Place an order")
 async def upload_order(order: Order):
     LOG.info("upload_order called")
     status_code = 201
